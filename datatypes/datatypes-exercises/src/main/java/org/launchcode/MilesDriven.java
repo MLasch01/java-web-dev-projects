f37b545b530216c96e423519daad7765d7690854
mlasch01 <mark.lasch@yahoo.com>
2024-09-03 17:43:27 -0500

    09/03/24

--- datatypes/datatypes-exercises/src/main/java/org/launchcode/MilesDriven.java
+++ datatypes/datatypes-exercises/src/main/java/org/launchcode/MilesDriven.java
@@ -1,4 +1,18 @@
 package org.launchcode;
 
+import java.util.Scanner;
+
 public class MilesDriven {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("How many miles have you driven");
+        Double miles = input.nextDouble();
+
+        System.out.println("How many gallons of gas have you consumed");
+        Double gallons = input.nextDouble();
+        input.close();
+
+        System.out.println("Your gas mileage is: " + miles/gallons + "MPG");
+    }
 }
