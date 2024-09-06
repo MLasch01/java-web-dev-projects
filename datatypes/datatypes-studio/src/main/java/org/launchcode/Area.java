6ad043544954f68d6fff7720286f674719890a5f
mlasch01 <mark.lasch@yahoo.com>
2024-09-05 20:26:22 -0500

    09-05-24

--- datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
+++ datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
@@ -6,15 +6,15 @@ public class Area {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        System.out.println("Enter a radius:");
-        Double radius = input.nextDouble();
-//        Double areaCircle = 3.14 * radius * radius;
-        Double areaCircle = Circle.getArea(radius);
-        input.close();
-
-        System.out.println("The area of the circle of radius " + radius + "is " + areaCircle);
-    }
-}
+//        System.out.println("Enter a radius:");
+//        Double radius = input.nextDouble();
+////        Double areaCircle = 3.14 * radius * radius;
+//        Double areaCircle = Circle.getArea(radius);
+//        input.close();
+//
+//        System.out.println("The area of the circle of radius " + radius + "is " + areaCircle);
+//    }
+//}
 
 
 //        Bonus Mission 1:
@@ -41,22 +41,22 @@ public class Area {
 
 
 //Bonus Mission 2:
-//            Double radius;
-//
-//        do {
-//            System.out.println("Enter a positive number for the radius:");
-//
-//             while (!input.hasNextDouble()) {
-//                System.out.println("The radius must be a positive number.  Please try again.");
-//                input.next();
-//             }
-//             radius = input.nextDouble();
-//
-//        } while (radius < 0);
-//
-//        Double areaCircle = Circle.getArea(radius);
-//        System.out.println("The area of the circle of radius " + radius + "is " + areaCircle);
-//
-//        input.close();
-//    }
-//}
+            Double radius;
+
+        do {
+            System.out.println("Enter a positive number for the radius:");
+
+             while (!input.hasNextDouble()) {
+                System.out.println("The radius must be a positive number.  Please try again.");
+                input.next();
+             }
+             radius = input.nextDouble();
+
+        } while (radius < 0);
+
+        Double areaCircle = Circle.getArea(radius);
+        System.out.println("The area of the circle of radius " + radius + "is " + areaCircle);
+
+        input.close();
+    }
+}
