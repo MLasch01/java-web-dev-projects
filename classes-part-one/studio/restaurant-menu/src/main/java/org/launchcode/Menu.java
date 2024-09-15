69792955c2b93c414087eb218aca6ec93429b046
mlasch01 <mark.lasch@yahoo.com>
2024-09-15 14:08:29 -0500

    09/15/24

--- classes-part-one/studio/restaurant-menu/src/main/java/org/launchcode/Menu.java
+++ classes-part-one/studio/restaurant-menu/src/main/java/org/launchcode/Menu.java
@@ -1,4 +1,13 @@
 package org.launchcode;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+
 public class Menu {
+    private ArrayList<MenuItem> menuItemsArrayList = new ArrayList<>();
+    private LocalDate lastUpdate;
+
+    public Menu(ArrayList<MenuItem> menuItemsArrayList) {
+        this.menuItemsArrayList = menuItemsArrayList;
+    }
 }
